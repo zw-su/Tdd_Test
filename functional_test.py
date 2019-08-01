b7d3731f222bc6f25cebe54e216c350b0a421ba6
zw <zouhero365@163.com>
2019-08-01 22:03:48 +0800

    Functional test now checks we can input somedata

--- functional_test.py
+++ functional_test.py
@@ -4,6 +4,8 @@
 
 from selenium import webdriver
 import unittest
+import time
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -21,27 +23,42 @@ class NewVisitorTest(unittest.TestCase):
         # 她注意到网页的标题和头部都包含"To-Do"这个词
         # assert "To-Do" in driver.title
         self.assertIn("To-Do", self.driver.title)
+        header_text = self.driver.find_element_by_tag_name('h1').text
         # 如 assertEqual 、assertTrue 和assertFalse 等。
-        self.fail('Finish the test!')
-        # 不管怎样,self.fail 都会失败，生成指定的错误消息。我使用这个方法提醒测试结束了
+        self.assertIn('To-Do', header_text)
+
         # 应用邀请她输入一个待办事项
+        inputbox = self.driver.find_element_by_id("id_input")
+        self.assertEqual(inputbox.get_attribute('placeholder'),
+                         'Enter a to-do item')
+        # placeholder是html5新增的一个属性，当input或者textarea设置了该属性后，
+        # 该值的内容将作为灰字提示显示在文本框中，当文本框获得焦点（或输入内容)时提示文字消失.
 
-        # 她在文本框里输入"Buy peacock feathers"(购买孔雀羽毛)
+        # 她在文本框里输入"Buy peacock feathers"(购买孔雀羽毛).
         # 爱吃素的爱好是使用假蝇做饵钓鱼
+        inputbox.send_keys("Buy peacock feathers")
 
-# 她按回车键后，页面更新了
-# 待办事项表格中显示了"1: Buy peacock feathers"
+        # 她按回车键后，页面更新了
+        # 待办事项表格中显示了"1: Buy peacock feathers"
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-# 页面中又显示了一个文本框，可以输入其他的待办事项
-# 她输入了"Use peacock feathers to make a fly"
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers'
+                            for row in rows))
 
-# 爱吃素做事很有条理
+        # 页面中又显示了一个文本框，可以输入其他的待办事项
+        # 她输入了"Use peacock feathers to make a fly"
+        self.fail('Finish the test')
 
-# 页面再次更新, 她的清单中显示了这两个待办事项
+        # 爱吃素做事很有条理
 
-# 爱吃素想指定这个网战是否会记住她的清单
-# 她看到网站为她生成了一个唯一的URL
-# 而且页面中有一些文字解说这个功能
+        # 页面再次更新, 她的清单中显示了这两个待办事项
+
+        # 爱吃素想指定这个网战是否会记住她的清单
+        # 她看到网站为她生成了一个唯一的URL
+        # 而且页面中有一些文字解说这个功能
 
 # 她访问那个URL,发现她的待办事项列表还在
 
