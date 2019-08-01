02ff17605486f4d118334e0da927a2fb87089d5e
zw <zouhero365@163.com>
2019-08-01 18:12:38 +0800

    First unit test and url mapping

--- functional_test.py
+++ functional_test.py
@@ -3,11 +3,55 @@
 # 第一个TDD测试的脚本
 
 from selenium import webdriver
+import unittest
 
-driver = webdriver.Chrome()
-driver.get("http://localhost:8000/")
-assert "Django" in driver.title
-driver.close()
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_can_start(self):
+        # 爱吃素听说有一个很酷的在线代办事项应用
+        # 她去看了这个应用的首页
+        self.driver.get("http://localhost:8000/")
+
+        # 她注意到网页的标题和头部都包含"To-Do"这个词
+        # assert "To-Do" in driver.title
+        self.assertIn("To-Do", self.driver.title)
+        # 如 assertEqual 、assertTrue 和assertFalse 等。
+        self.fail('Finish the test!')
+        # 不管怎样,self.fail 都会失败，生成指定的错误消息。我使用这个方法提醒测试结束了
+        # 应用邀请她输入一个待办事项
+
+        # 她在文本框里输入"Buy peacock feathers"(购买孔雀羽毛)
+        # 爱吃素的爱好是使用假蝇做饵钓鱼
+
+# 她按回车键后，页面更新了
+# 待办事项表格中显示了"1: Buy peacock feathers"
+
+# 页面中又显示了一个文本框，可以输入其他的待办事项
+# 她输入了"Use peacock feathers to make a fly"
+
+# 爱吃素做事很有条理
+
+# 页面再次更新, 她的清单中显示了这两个待办事项
+
+# 爱吃素想指定这个网战是否会记住她的清单
+# 她看到网站为她生成了一个唯一的URL
+# 而且页面中有一些文字解说这个功能
+
+# 她访问那个URL,发现她的待办事项列表还在
+
+# 她很满意，去睡觉了
+
 
 # driver.close()关闭当前窗口
 # driver.quit()退出驱动关闭所有窗口
+
+if __name__ == "__main__":
+    # unittest.main(warnings='ignore')
+    # warnings='ignore'的作用是禁止抛出ResourceWarning 异常。
+    unittest.main()
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,9 @@
+# coding = utf-8
+
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home_page():
+	pass
