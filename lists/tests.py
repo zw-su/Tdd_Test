1c2b42c97025d21f3b4df4c399abf19cf4dd37a1
zw <zouhero365@163.com>
2019-08-02 08:53:22 +0800

    use render_to_string check page rendering is correct

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 # Create your tests here.
 
 
@@ -17,6 +18,5 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
--- lists/views.py
+++ lists/views.py
@@ -7,4 +7,4 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+	return render(request, 'home.html')
