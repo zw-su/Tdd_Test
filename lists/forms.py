3e5dc6403f23d0efad4e650f6526545d776c2b5d
zw <zouhero365@163.com>
2019-08-17 12:27:52 +0800

    为待办事项新增了新的表单验证

---
+++ lists/forms.py
@@ -0,0 +1,23 @@
+# coding = utf-8
+
+from django import forms
+from lists.models import Item
+
+EMPTY_ITEM_ERROR = "你不能输入一个空的待办事项"
+INPUT_PLACEHOLDER = "输入你想要做的事情"
+
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': INPUT_PLACEHOLDER,
+                'class': 'form-control input-lg',
+            })
+        }
+        error_messages = {
+            'text': {'required': EMPTY_ITEM_ERROR}
+        }
---
+++ lists/tests/test_forms.py
@@ -0,0 +1,24 @@
+# coding = utf-8
+
+from django.test import TestCase
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR, INPUT_PLACEHOLDER
+
+'''表单的单元测试'''
+
+
+class ItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        form = ItemForm()
+        print(form.as_p())
+
+    def test_form_item_input_has_placeholder_and_css(self):
+        form = ItemForm()
+        self.assertIn(f'placeholder="{INPUT_PLACEHOLDER}"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+        # form.save()
