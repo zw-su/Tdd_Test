c5f26f007e38a06fa4d3e419b41a7750a56e1344
zw <zouhero365@163.com>
2019-08-19 19:03:58 +0800

    增加jquery,qunit tests,list.js消除错误提示信息

--- functional_tests/test_listitem_validation.py
+++ functional_tests/test_listitem_validation.py
@@ -11,6 +11,9 @@ from selenium.webdriver.common.by import By
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.driver.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # 爱吃素访问首页，不小心提交了一个空待办事项
         # 输入框中没输入内容，她就按下了回车键
@@ -24,7 +27,7 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for(
             lambda: self.driver.find_element_by_css_selector('#id_text:invalid'))
 
-        # self.assertEqual(self.driver.find_element_by_css_selector('.has-error'),
+        # self.assertEqual(self.get_error_element(),
         # "你不能输入一个空的待办事项")
 
         # 她输入一些文字，然后再次提交，这次没问题了
@@ -62,9 +65,24 @@ class ItemValidationTest(FunctionalTest):
 
         # 她看到一条有帮助的错误消息
         self.wait_for(lambda: self.assertEqual(
-            self.driver.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             '你已经有这个待办事项了'))
 
     def test_error_messages_are_cleared(self):
         # 爱吃素新建了一个清单，但方法不当，所以出现了一个验证错误
-        pass
+        self.driver.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.check_rowtext_in_listTable('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()))
+
+        # 为了消除错误，她开始在输入框中输入内容
+        self.get_item_input_box().send_keys('a_test')
+
+        # 看到错误消息消失了，她很高兴
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()))
