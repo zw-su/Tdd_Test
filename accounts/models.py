48d06eaf4b1a140d1bf3bda1b1328712c08d5950
zw <zouhero365@163.com>
2019-08-20 23:47:07 +0800

    重新在master建立新app，accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,34 +1,3 @@
-# coding = utf-8
-
 from django.db import models
-from django.contrib.auth.models import (
-    AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
-
-class Token(models.Model):
-    email = models.EmailField(default='')
-    uid = models.CharField(max_length=255, default='')
-
-
-class ListUserManager(BaseUserManager):
-
-    def create_user(self, email):
-        ListUser.objects.create(email=email)
-
-    def create_superuser(self, email, password):
-        self.create_user(email)
-
-
-class ListUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(primary_key=True)
-    USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = ['email', 'height']
-    objects = ListUserManager()
-
-    @property
-    def is_staff(self):
-        return self.email == "zouhero365@163.com"
-
-    @property
-    def is_active(self):
-        return True
+# Create your models here.
